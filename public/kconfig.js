f28c30d41fbae90adf560e83ee51995cdbbcbc75
Furkan Erdem <furkan.erdem@jekirdek.com>
2025-11-21 15:38:40 +0300

    feat: vite konfigürasyonun compose'a göre güncelle

--- public/kconfig.js
+++ public/kconfig.js
@@ -1,7 +1,7 @@
 window.K_CONFIG = {
-    'ADMIN_GUI_PATH': '/',
-    'ADMIN_API_URL': 'https://adminapi.securesoft.tr',
-    'ADMIN_API_PORT': '8001',
-    'ADMIN_API_SSL_PORT': '',
-    'ANONYMOUS_REPORTS': 'true'
-  }
+  'ADMIN_GUI_PATH': '/',
+  'ADMIN_API_URL': 'https://mysite.example:2096',
+  'ADMIN_API_PORT': '2096',
+  'ADMIN_API_SSL_PORT': '2096',
+  'ANONYMOUS_REPORTS': 'false'
+}
